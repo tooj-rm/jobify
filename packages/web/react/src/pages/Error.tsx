3a7c79ca75f21e45a4f158d42324ea1b44493810
tooj <tjnyaina@gmail.com>
2025-07-20 15:35:09 +0300

    [feat] react: error page

--- packages/web/react/src/pages/Error.tsx
+++ packages/web/react/src/pages/Error.tsx
@@ -1,7 +1,25 @@
+import { Link, useRouteError } from 'react-router';
+import img from '~/assets/images/not-found.svg';
+
 const Error = () => {
+  const error = useRouteError();
+  console.log(error);
+
   return (
-    <div>
-      Error
+    <div className="flex flex-col items-center justify-center min-h-[100vh]">
+      {
+        // @ts-ignore
+        (error.status === 404) ? (
+          <>
+            <img src={img} alt="not found" className="max-w-[600px] max-h-[300px] mb-[2rem]"/>
+            <h3>Page not found</h3>
+            <p className="text-grey-500 mb-[1rem]">we can't seem to find the page you are looking for</p>
+            <Link to="/" className="text-primary-500">Back Home</Link>
+          </>
+        ) : (
+          <h3>Something went wrong</h3>
+        )
+      }
     </div>
   );
 };
